a0bdf871fce491f9a49a4a40a907bed3ef201931
evgner <evgner@gmail.com>
2019-02-03 22:45:21 +0600

    Add Logo and Tilt.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +8,14 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Navigation />
-				{/*<Logo />
+				<Logo />
+				{/*
 				<ImageLinkForm />
-				<FaceRecognition />*/}
+				<FaceRecognition />
+				*/}
+				{/*
+				<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+				*/}
 			</div>
 		);
 	}
